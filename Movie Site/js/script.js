f8f0e843c529147def062f0c772f41ecb31836da
Karl George <kelsyerg@gmail.com>
2017-05-04 18:20:17 +0100

    Movie Site- Responsiveness

--- Movie Site/js/script.js
+++ Movie Site/js/script.js
@@ -1,6 +1,14 @@
 var menu = document.querySelector(".menu");
 
+// Click the bars to assign an active class to main_nav which will show navigation menu
 menu.addEventListener('click', function(e) {
     e.stopPropagation();
     document.querySelector("#main_nav").classList.toggle("active"); 
 });
+
+
+// Click anywhere on the site to remove the active class, closing the nav bar
+document.addEventListener('click', function() {
+     document.querySelector("#main_nav").classList.remove("active"); 
+}); 
+
